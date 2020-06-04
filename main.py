a76476520a14a27d1e59b6aa0cf6ea3772fa4d53
SweeetNik <59518348+SweeetNik@users.noreply.github.com>
2020-06-04 23:47:27 +0300

    пап

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ user_password = '1029384756Nikersss'
 session = vk.AuthSession(scope='wall', app_id=my_app_id, user_login=user_login, user_password=user_password)
 vk.api.access_token="e47a840ce7af354b18208aac442f84ba6512644590bc55cb5a643e97282b78d72a6c4479271b6f9a7ca3d"
 api = vk.API(session)
-a = 38
+a = 36
 while a > 1:
     api.wall.post(owner_id='-195777168', message="Димас еще в армии\nДимасу осталось служить " + str(a) + " дней", attachments='photo201342171_456272777', v=5.107)
     print(a)
