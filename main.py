76d1f4345ac753a34c6be2c1fca77ff0abfe1141
SweeetNik <59518348+SweeetNik@users.noreply.github.com>
2020-06-19 22:35:32 +0300

    update gug

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 # -*- coding: utf-8 -*-
 import vk
 import time
+
 my_app_id = 7496109
 user_login = '+79883214673'
 user_password = '1029384756Nikersss'
 
-session = vk.AuthSession(scope='wall', app_id=my_app_id, user_login=user_login, user_password=user_password)
-vk.api.access_token="e47a840ce7af354b18208aac442f84ba6512644590bc55cb5a643e97282b78d72a6c4479271b6f9a7ca3d"
-api = vk.API(session)
-a = 36
-while a > 1:
+
+def subtract():
+    global a
     a -= 1
-    api.wall.post(owner_id='-195777168', message="Димас еще в армии\nДимасу осталось служить " + str(a) + " дней", attachments='photo201342171_456272777', v=5.107)
+    api.wall.post(owner_id='-195777168', message="Димас еще в армии\nДимасу осталось служить " + str(a) + " дней",
+                  attachments='photo201342171_456272777', v=5.107)
     print(a)
     time.sleep(86400)
     if a == 1:
-        api.wall.post(owner_id='-195777168', message="Димас ДЕМБЕЛЬНУЛСЯ", attachments='photo201342171_456272777', v=5.107)
-        break
-    
+        api.wall.post(owner_id='-195777168', message="Димас ДЕМБЕЛЬНУЛСЯ", attachments='photo201342171_456272777',
+                      v=5.107)
+
+
+session = vk.AuthSession(scope='wall', app_id=my_app_id, user_login=user_login, user_password=user_password)
+vk.api.access_token = "e47a840ce7af354b18208aac442f84ba6512644590bc55cb5a643e97282b78d72a6c4479271b6f9a7ca3d"
+api = vk.API(session)
+a = 21
+while a > 1:
+    try:
+        subtract()
+    except Exception as E:
+        time.sleep(5)
+        subtract()
